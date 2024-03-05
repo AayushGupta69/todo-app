95582dc632bee289dbd1cd622a8fb8d3a89e92c1
Aayush Gupta <aayushgupta69official@gmail.com>
2024-03-06 00:05:00 +0530

    Todo backend done

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,43 @@
 const express = require("express");
 const {createTodo, updateTodo} = require("./types");
+const {todo} = require("./db");
 const app = express();
 
 app.use(express.json());
 
-app.post("/createTodos", (req, res) => {
+app.post("/createTodos", async (req, res) => {
     const payload = req.body;
     const inputs = createTodo.safeParse(payload);
     if(!inputs.success){
         res.status(411).json({message: "Wrong Inputs!"});
     }
+
+    await todo.create({
+        title: payload.title,
+        description: payload.description,
+        completed: false
+    });
+
+    res.status(200).json({message: "Todo Created!"});
 })
 
-app.get("/todos", (req, res) => {
-
+app.get("/todos", async (req, res) => {
+    const allTodos = await todo.find({});
+    res.status(200).json({allTodos});
 })
 
-app.put("/updateTodo", (req, res) => {
+app.put("/updateTodo", async (req, res) => {
     const payload = req.body;
     const inputs = updateTodo.safeParse(payload);
     if(!inputs.success){
         res.status(411).json({message: "Wrong Inputs!"});
     }
+
+    await todo.updateOne({
+        _id: payload.id
+    }, {
+        completed: true
+    })
+
+    res.status(200).json({message: "Marked todo as completed!"});
 })
