20b36fbb9fe55cdd3d0036d2e6c6e2a3c544bbc4
Aayush Gupta <aayushgupta69official@gmail.com>
2024-03-06 22:13:41 +0530

    Completed (almost)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import {CreateTodo} from "./Components/CreateTodo.jsx";
 import {Todos} from "./Components/Todos.jsx";
 
 function App() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+            fetch("http://localhost:3000/todos").then(async function(res){
+                const json = await res.json();
+                setTodos(json.allTodos);
+            })
+  }, [])
 
   return (
     <div>
-        <CreateTodo/>
-        <Todos/>
+        <CreateTodo />
+        <Todos todos={todos}/>
     </div>
   )
 }
--- frontend/src/Components/Todos.jsx
+++ frontend/src/Components/Todos.jsx
@@ -1,12 +1,20 @@
-export function Todos(){
+export function Todos({todos}){
     return (
         <div>
-            <h1> Go to Gym</h1>
-            <h2> Go to Gym from 7 - 9</h2>
-            <button style={{
-                padding: 10,
-                margin: 10
-            }}>Mark as completed</button>
+            {todos.map(function(todo) {
+                return(
+                    // eslint-disable-next-line react/jsx-key
+                    <div key = {todo.id}>
+                        <h1>{todo.title}</h1>
+                        <h2>{todo.description}</h2>
+                        <button style={{
+                            padding: 10,
+                            margin: 10
+                        }}>{todo.completed === true ? "Completed" : "Mark as Completed"}
+                        </button>
+                    </div>
+                )
+            })}
         </div>
     )
 }
